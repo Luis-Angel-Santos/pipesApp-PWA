deb2d6eb29a003eaa286c39577d601c7423a1674
Luis Angel Santos <santosangel748@gmail.com>
2022-09-03 18:48:57 -0500

    Trabajando con elos pipes DecimalPipe y CurrencyPipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -5,11 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './numeros.component.html',
   styleUrls: ['./numeros.component.css']
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NumerosComponent  {
+  ventasNetas: number = 25323313.3209;
+  porcentaje: number = 0.6998;
+ 
 
 }
